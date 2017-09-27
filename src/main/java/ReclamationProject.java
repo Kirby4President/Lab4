275205757943e133e032cbbeaf893b9324b62337
amissa2 <amissa2@siebl-0218-30.ews.illinois.edu>
2017-09-27 14:25:32 -0500

    Help me, goddamnit!

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -12,15 +12,19 @@ public class ReclamationProject
 {
     static String doit(String a,String b){
         if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
-         */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+            String c = a;
+            a=b;
+            b=c;
+        }
+        String r = /* (a.equals(b)) ? "" : */ "";
+        for (int iCounter = 0; iCounter < a.length(); iCounter++){
+            for (int jCounter = a.length() - iCounter; jCounter > 0; jCounter--) {
+                for (int kCounter = 0; kCounter < b.length()- jCounter; kCounter++) {
+                    boolean regionMatch = a.regionMatches(iCounter, b, kCounter, jCounter);
+                    r = (regionMatch && jCounter > r.length()) ? a.substring(iCounter,iCounter + jCounter) : r; // Setting R to the largest substring from both a and b
+                }
+            }
+        }
+        return r;
+    }
 }
